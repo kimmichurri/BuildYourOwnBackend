84618d26eb5b4f2bbc9c602771f33a6ae34b2e98
“Kimberly <kimberly.achkio@gmail.com>
2019-05-01 09:51:20 -0600

    Run first seed

--- db/seeds/dev/artists.js
+++ db/seeds/dev/artists.js
@@ -1,13 +1,19 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('artists').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return Promise.all([
+        knex('artists').insert({
+          name: 'Fernando Botero', nationality: 'Colombian'
+        })
+        .then(() => console.log('Seeding successful!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+      // return knex('table_name').insert([
+      //   {id: 1, colName: 'rowValue1'},
+      //   {id: 2, colName: 'rowValue2'},
+      //   {id: 3, colName: 'rowValue3'}
+      // ]);
     });
 };
