c41b39d1db278e9e25d96392f291f5006613144c
“Kimberly <kimberly.achkio@gmail.com>
2019-05-01 11:46:49 -0600

    Add data to tables

--- db/seeds/dev/artists.js
+++ db/seeds/dev/artists.js
@@ -5,6 +5,25 @@ const createArtist = (knex, artist) => {
     name: artist.name,
     nationality: artist.nationality
   }, 'id')
+    .then(artistId => {
+      let artworkPromises = [];
+
+      artist.artworks.forEach(artwork => {
+        artworkPromises.push(
+          createArtwork(knex, {
+            title: artwork.title,
+            date: artwork.date,
+            img_url: artist.img_url,
+            artist_id: artistId[0]
+          })
+        )
+      });
+      return Promise.all(artworkPromises);
+    })
+}
+
+const createArtwork = (knex, artwork) => {
+  return knex('artworks').insert(artwork);
 }
 
 exports.seed = function(knex, Promise) {
