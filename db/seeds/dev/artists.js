558e3a86699e2a1ef8a8e3ca69346cb57f609874
“Kimberly <kimberly.achkio@gmail.com>
2019-05-01 10:12:40 -0600

    Run seed to populate artists in database

--- db/seeds/dev/artists.js
+++ db/seeds/dev/artists.js
@@ -1,19 +1,22 @@
+const artists = require('../../../artists');
+
+const createArtist = (knex, artist) => {
+  return knex('artists').insert({
+    name: artist.name,
+    nationality: artist.nationality
+  }, 'id')
+}
+
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('artists').del()
     .then(function () {
-      // Inserts seed entries
-      return Promise.all([
-        knex('artists').insert({
-          name: 'Fernando Botero', nationality: 'Colombian'
-        })
-        .then(() => console.log('Seeding successful!'))
-        .catch(error => console.log(`Error seeding data: ${error}`))
-      ])
-      // return knex('table_name').insert([
-      //   {id: 1, colName: 'rowValue1'},
-      //   {id: 2, colName: 'rowValue2'},
-      //   {id: 3, colName: 'rowValue3'}
-      // ]);
-    });
+      let artistPromises = [];
+
+      artists.forEach(artist => {
+        artistPromises.push(createArtist(knex, artist));
+      });
+      return Promise.all(artistPromises);
+    })
+    .then(() => console.log('Seeding successful!'))
+    .catch(error => console.log(`Error seeding data: ${error}`))
 };
