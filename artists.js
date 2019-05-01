43837c7a4daecc5177d6947e8c14d15d02be76e2
“Kimberly <kimberly.achkio@gmail.com>
2019-05-01 10:12:06 -0600

    Break out data into separate files

---
+++ artists.js
@@ -0,0 +1,45 @@
+//consider including paintings as a key on each artist object and then move all of those objects over from paintings array
+const artists = [
+  {
+    "name": "Paul Gauguin",
+    "nationality": "French"
+  },
+  {
+    "name": "Paul Cézanne",
+    "nationality": "French"
+  },
+  {
+    "name": "Edgar Degas",
+    "nationality": "French"
+  },
+  {
+    "name": "Edvard Munch",
+    "nationality": "Norwegian"
+  },
+  {
+    "name": "Fernando Botero",
+    "nationality": "Colombian"
+  },
+  {
+    "name": "Hiroshige",
+    "nationality": "Japanese"
+  },
+  {
+    "name": "Hokusai",
+    "nationality": "Japanese"
+  },
+  {
+    "name": "Henri Rousseau",
+    "nationality": "French"
+  },
+  {
+    "name": "Edward Hopper",
+    "nationality": "American"
+  },
+  {
+    "name": "Andy Warhol",
+    "nationality": "American"
+  }
+]
+
+module.exports = artists;
