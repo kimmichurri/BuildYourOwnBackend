bd0971d1a621b4d49567a683bfe14f759eb918e7
“Kimberly <kimberly.achkio@gmail.com>
2019-05-02 14:16:59 -0600

    Update GETs for sad path

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.listen(port, () => console.log(`App is listening on port ${port}`));
 app.get('/api/v1/artists', (request, response) => {
   database('artists').select()
     .then((artists) => {
-      response.status(200).json(artists)
+      if (artists.length) {
+        response.status(200).json(artists)
+      } else {
+        response.status(404).json({
+          error: "There currently aren't any artists in the database"
+        })
+      }
     })
     .catch((error) => {
       response.status(500).json({ error })
@@ -51,27 +57,6 @@ app.get('/api/v1/artists/:id/artworks', (request, response) => {
     })
 })
 
-app.post('/api/v1/artists/:id/artworks', (request, response) => {
-  const artwork = request.body;
-  const artistId = request.params.id;
-
-  for (let requiredParameter of ['title', 'date', 'img_url']) {
-    if(!artwork[requiredParameter]){
-      return response
-        .status(422)
-        .send({ error: `Expected format: { title: <String>, date: <Integer>, img_url: <String> } You're missing a "${requiredParameter}" property.` })
-    }
-  }
-
-  database('artworks').insert({ ...artwork, artist_id: artistId }, 'id')
-    .then(artwork => {
-      response.status(201).json( `You successfully added a new piece of art!` )
-    })
-    .catch(error => {
-      response.status(500).json({ error })
-    })
-})
-
 app.post('/api/v1/artists', (request, response) => {
   const artist = request.body;
 
@@ -79,7 +64,7 @@ app.post('/api/v1/artists', (request, response) => {
     if(!artist[requiredParameter]) {
       return response 
         .status(422)
-        .send({ error: `Expected format: { name: <String>, author: <String> } You're missing a "${requiredParameter}" property.` })
+        .send({ error: `Expected format: { name: <String>, nationality: <String> } You're missing a "${requiredParameter}" property.` })
     }
   }
 
@@ -95,7 +80,13 @@ app.post('/api/v1/artists', (request, response) => {
 app.get('/api/v1/artworks', (request, response) => {
   database('artworks').select()
     .then((artworks) => {
-      response.status(200).json(artworks)
+      if (artworks.length) {
+        response.status(200).json(artworks)
+      } else {
+        response.status(404).json({
+          error: "There currently aren't any artworks in the database"
+        })
+      }
     })
     .catch((error) => {
       response.status(500).json
@@ -118,11 +109,32 @@ app.get('/api/v1/artworks/:id', (request, response) => {
     })
 })
 
+app.post('/api/v1/artists/:id/artworks', (request, response) => {
+  const artwork = request.body;
+  const artistId = request.params.id;
+
+  for (let requiredParameter of ['title', 'date', 'img_url']) {
+    if(!artwork[requiredParameter]){
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, date: <Integer>, img_url: <String> } You're missing a "${requiredParameter}" property.` })
+    }
+  }
+
+  database('artworks').insert({ ...artwork, artist_id: artistId }, 'id')
+    .then(artwork => {
+      response.status(201).json( `You successfully added a new piece of art!` )
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
 app.delete('/api/v1/artworks/:id', (request, response) => {
   database('artworks').where('id', request.params.id).select().del()
     .then(artwork => {
       if (artwork) {
-        response.status(201).json({ message: `The artwork with id ${request.params.id} was successfully deleted.`})
+        response.status(200).json({ message: `The artwork with id ${request.params.id} was successfully deleted.`})
       } else {
         response.status(404).json({ error: `We could not find an artwork with id of ${request.params.id} to delete.`})
       }
